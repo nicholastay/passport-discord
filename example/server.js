f9a4584307173d92c68d5a085bd21978352f099e
Hussain Aqeel <69085853+huss-a@users.noreply.github.com>
2021-05-16 11:42:30 +0300

    Modern JavaScript
    Use of `const` instead of `var` and use of arrow function syntax (`=>`)

--- example/server.js
+++ example/server.js
@@ -1,18 +1,18 @@
-var express  = require('express')
+const express  = require('express')
   , session  = require('express-session')
   , passport = require('passport')
   , Strategy = require('../lib').Strategy
   , app      = express();
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser((user, done) => {
   done(null, user);
 });
-passport.deserializeUser(function(obj, done) {
+passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
 
-var scopes = ['identify', 'email', /* 'connections', (it is currently broken) */ 'guilds', 'guilds.join'];
-var prompt = 'consent'
+const scopes = ['identify', 'email', /* 'connections', (it is currently broken) */ 'guilds', 'guilds.join'];
+const prompt = 'consent'
 
 passport.use(new Strategy({
     clientID: '',
@@ -20,8 +20,8 @@ passport.use(new Strategy({
     callbackURL: 'http://localhost:5000/callback',
     scope: scopes,
     prompt: prompt
-}, function(accessToken, refreshToken, profile, done) {
-    process.nextTick(function() {
+}, (accessToken, refreshToken, profile, done) => {
+    process.nextTick(() => {
         return done(null, profile);
     });
 }));
@@ -33,27 +33,27 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.get('/', passport.authenticate('discord', { scope: scopes, prompt: prompt }), function(req, res) {});
+app.get('/', passport.authenticate('discord', { scope: scopes, prompt: prompt }), (req, res) => {});
 app.get('/callback',
-    passport.authenticate('discord', { failureRedirect: '/' }), function(req, res) { res.redirect('/info') } // auth success
+    passport.authenticate('discord', { failureRedirect: '/' }), (req, res) => { res.redirect('/info') } // auth success
 );
-app.get('/logout', function(req, res) {
+app.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
 });
-app.get('/info', checkAuth, function(req, res) {
+app.get('/info', checkAuth, (req, res) => {
     //console.log(req.user)
     res.json(req.user);
 });
 
 
-function checkAuth(req, res, next) {
+const checkAuth = (req, res, next) => {
     if (req.isAuthenticated()) return next();
     res.send('not logged in :(');
 }
 
 
-app.listen(5000, function (err) {
+app.listen(5000, err => {
     if (err) return console.log(err)
     console.log('Listening at http://localhost:5000/')
 })
